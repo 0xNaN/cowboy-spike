a9ad83c69c2649f13cb20713c61bc065d7e736cd
0xNaN <enzodicicco@gmail.com>
2015-11-19 15:23:27 +0100

    adding a first handler

--- apps/myapp/src/myapp_app.erl
+++ apps/myapp/src/myapp_app.erl
@@ -11,11 +11,19 @@
 -export([start/2
         ,stop/1]).
 
+-define(C_ACCEPTORS, 100).
+
 %%====================================================================
 %% API
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Routes = routes(),
+    Dispatch = cowboy_router:compile(Routes),
+    Port = port(),
+    TransOpts = [{port, Port}],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+    {ok, _}   = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
     'myapp_sup':start_link().
 
 %%--------------------------------------------------------------------
@@ -25,3 +33,19 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+routes() ->
+    [
+     {'_', [
+            {"/", my_app_handler, []}
+           ]}
+    ].
+
+port() ->
+    case os:getenv("PORT") of
+        false ->
+            {ok, Port} = application:get_env(http_port),
+            Port;
+        Other ->
+            list_to_integer(Other)
+    end.
